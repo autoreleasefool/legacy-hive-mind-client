361f3a5cf5da7cc98b8b0eca59738fd456d2f149
Joseph Roque <contact@josephroque.ca>
2019-03-03 11:17:26 -0500

    Make requests for API and call delegates on result

--- HiveAI/HiveAI/Source/API/HiveAPI.swift
+++ HiveAI/HiveAI/Source/API/HiveAPI.swift
@@ -9,12 +9,19 @@
 import UIKit
 import HiveEngine
 
+enum ApiError: Error {
+	case noData
+	case decodingFailure
+	case dataError
+}
+
 protocol HiveApiDelegate: class {
 	func didPlay(api: HiveApi, move: Movement)
 	func didBeginGame(api: HiveApi)
+	func didReceiveError(api: HiveApi, error: Error)
 }
 
-struct HiveApi: Codable {
+class HiveApi: Codable {
 
 	let name: String
 	let description: String
@@ -37,11 +44,68 @@ struct HiveApi: Codable {
 		return endpointURL.appendingPathComponent("new")
 	}
 
-	func play(in state: GameState, completion: @escaping (Movement) -> Void) {
+	func play(in state: GameState, delegate: HiveApiDelegate) {
+		let encoder = JSONEncoder()
+		let data: Data
+		do {
+			data = try encoder.encode(state)
+		} catch {
+			delegate.didReceiveError(api: self, error: error)
+			return
+		}
 
+		var request = URLRequest(url: playURL)
+		request.httpMethod = "POST"
+		request.httpBody = data
+		request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+		let task = URLSession.shared.dataTask(with: request) { [weak self, weak delegate] data, _, error in
+			guard let self = self, let delegate = delegate else { return }
+			self.parseApiResponse(data: data, error: error, delegate: delegate)
+		}
+		task.resume()
 	}
 
-	func newGame(playerIsFirst: Bool, completion: @escaping (Movement?) -> Void) {
+	func newGame(playerIsFirst: Bool, delegate: HiveApiDelegate) {
+		let jsonString = "{\"playerIsFirst\": \(playerIsFirst)}"
+		guard let data = jsonString.data(using: .utf8) else {
+			delegate.didReceiveError(api: self, error: ApiError.dataError)
+			return
+		}
 
+		var request = URLRequest(url :newGameURL)
+		request.httpMethod = "POST"
+		request.httpBody = data
+		request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+		let task = URLSession.shared.dataTask(with: request) { [weak self, weak delegate] data, _, error in
+			guard let self = self, let delegate = delegate else { return }
+			if playerIsFirst == false {
+				self.parseApiResponse(data: data, error: error, delegate: delegate)
+			} else {
+				delegate.didBeginGame(api: self)
+			}
+		}
+		task.resume()
+	}
+
+	private func parseApiResponse(data: Data?, error: Error?, delegate: HiveApiDelegate) {
+		if let error = error {
+			delegate.didReceiveError(api: self, error: error)
+			return
+		}
+
+		guard let data = data else {
+			delegate.didReceiveError(api: self, error: ApiError.noData)
+			return
+		}
+
+		do {
+			let decoder = JSONDecoder()
+			let reply = try decoder.decode(Movement.self, from: data)
+			delegate.didPlay(api: self, move: reply)
+		} catch {
+			delegate.didReceiveError(api: self, error: error)
+		}
 	}
 }
